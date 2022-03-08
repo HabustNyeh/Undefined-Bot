9804ff05d21071345579b43e14e4f24a048b3193
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2022-03-08 21:50:01 +0700

    Emror tipo

--- plugins/poke_claim.js
+++ plugins/poke_claim.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix}) => {
 	function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)] }
 	let pokemon = global.db.data.pokerole[m.sender]
-	if(pokemon.role = true){
+	if(pokemon.claim = true){
 		conn.reply(m.chat, `-_-`, m)
 		return false
 	}
@@ -12,6 +12,7 @@ let handler = async (m, { conn, text, usedPrefix}) => {
     let [nama, kelamin, tipe, tipe1] = listpoke.split("|")
     let [tipe2, blank] = tipe1.split(">") 
     pokemon.claim = true
+    pokemon.role = true
     pokemon.jumlah += 1
    conn.reply(m.chat, `Selamat kamu mendapatkan ${nama} dengan kelamin ${kelamin}, Tipe 1 ${tipe}, Tipe 2 ${tipe2}, hanya bisa menggunakan tipe 2 jika ${nama} kamu naik level`, m) 
 }
