b58881bb58b82f3293d0b974487cfc8d5270a412
HyroKidsSmp <79112870+HyroKidsSmp@users.noreply.github.com>
2022-03-08 21:44:57 +0700

    Maybe pokemon

--- plugins/poke_inv.js
+++ plugins/poke_inv.js
@@ -1,23 +1,50 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text }) => {
     let pokemon = global.db.data.pokerole[m.sender]
-    let su = `Hai ${pokemon.nameteam}
+    let pokemonteam = pokemon.team
+    let pokemonlist = pokemon.list
+    let pokemonjumlah = pokemon.jumlah
+    if(pokemon.claim == false){
+    	conn.reply(m.chat, `Kamu belum memiliki Pokemon, Silahkan claim dulu`) 
+        return false
+    }
+    let [team1, team2, team3] = pokemonteam.split(">") 
+    if(pokemonjumlah == 1){
+    	pokemon.tierpoke2 = '~'
+        pokemon.xppoke2 = '~'
+        pokemon.item2 = '~'
+        team2 = '~'
+        pokemon.tierpoke3 = '~'
+        pokemon.xppoke3 = '~'
+        pokemon.item3 = '~'
+        team3 = '~'
+    }
+    if(pokemonjumlah == 2){
+        pokemon.tierpoke3 = '~'
+        pokemon.xppoke3 = '~'
+        pokemon.item3 = '~'
+        team3 = '~'
+    }
+    let su = `Hai ${global.db.data.users[m.sender].name}
           *${pokemon.nameteam}*
-*Team:* ${pokemon.team}
-*List Pokemon:* ${pokemon.list}
+*Team:* ${team1}, ${team2}, ${team3}
 *Item:* ${pokemon.item}
 
-*item pokemon 2:* ${pokemon.item1}
+*item pokemon 1:* ${pokemon.item1}
 *item pokemon 2:* ${pokemon.item2}
-*item pokemon 2:* ${pokemon.item3}
+*item pokemon 3:* ${pokemon.item3}
 
 *item XP 1:* ${pokemon.pokemonxp1}
 *item XP 2:* ${pokemon.pokemonxp2}
 *item XP 3:* ${pokemon.pokemonxp3}
 
-*Tier Pokemon 1*: ${pokemon.tier1}
-*Tier Pokemon 2*: ${pokemon.tier2}
-*Tier Pokemon 3*: ${pokemon.tier3}`
+*Tier Pokemon 1*: ${pokemon.tierpoke1}
+*Tier Pokemon 2*: ${pokemon.tierpoke2}
+*Tier Pokemon 3*: ${pokemon.tierpoke3}
+
+*Xp Pokemon 1*: ${pokemon.xppoke1}
+*Xp Pokemon 2*: ${pokemon.xppoke2}
+*Xp Pokemon 3*: ${pokemon.xppoke3}`
 
 conn.reply(m.chat, `${su}`) 
 }
